1a619e15f6c1b0602dc4b30f9a2da50397e3b0e5
milagrant <milag@kth.se>
2019-05-22 12:41:30 +0200

    breadth first search sampling

---
+++ BFS.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 22 10:32:39 2019
+
+@author: Mila
+"""
+import csv
+import random
+import queue
+
+FULL = 1134890
+SAMPLE = FULL *0.01
+fr = list()
+to = list()
+    
+def init():
+    global fr
+    global to
+    with open('ytgraph.txt.') as f:
+        reader = csv.reader(f, delimiter="\t")
+        first_col = list(zip(*reader))[0]
+        fr = list(map(int, first_col))
+    
+    with open('ytgraph.txt') as f:
+        reader = csv.reader(f, delimiter="\t")
+        second_col = list(zip(*reader))[1]
+        to = list(map(int, second_col))
+        
+def bfs(root):
+     Q = queue.Queue()
+     Q.put(root)
+     visited = list()     
+     visited.append(root)     
+     edges = list()
+     
+     while len(visited) < SAMPLE and not Q.empty():        
+         current = Q.get()
+         neighbours = list()
+         for i in range(FULL):
+             if fr[i] == current:
+                 neighbours.append(to[i])
+         for n in neighbours:
+             if n in visited:
+                 continue
+             Q.put(n)
+             visited.append(n)
+             edges.append([current, n])
+    
+     return edges
+    
+init()
+root = random.randint(1,FULL)
+edges = bfs(1)
+print(edges)
